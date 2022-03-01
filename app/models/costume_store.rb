f3a10d064782a11dcd03720f786ee8f5d42dde57
nathalia <nathaliatroina@gmail.com>
2022-03-01 10:16:02 -0500

    commit tree 297ab81bec4c85e33b8c4b85bbfffd827dcc382d

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,4 @@
 # Create your CostumeStore class here
+
+class CreateCostumeStores < ActiveRecord::Migration[6.1]
+end
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,4 @@
 # Create your HauntedHouse class here
+
+class CreateHauntedHouses < ActiveRecord::Migration[6.1]
+end
